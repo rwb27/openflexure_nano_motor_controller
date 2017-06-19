f93abc0f258efcf4db1b265ff6db43749bb44810
Richard Bowman <richard.bowman@cantab.net>
2017-06-19 18:29:33 +0300

    Working version with 3-axis move

--- arduino_code/openflexure_nano_motor_controller/openflexure_nano_motor_controller.ino
+++ arduino_code/openflexure_nano_motor_controller/openflexure_nano_motor_controller.ino
@@ -10,10 +10,14 @@
  * (c) Richard Bowman & James Sharkey, Released under GPL v3, 2017
  */
 #include <StepperF_alt.h>   //Fergus's hacked stepper library
+#include <assert.h>
+
+#define EACH_MOTOR for(int i=0; i<n_motors; i++)
 
 // The array below has 3 stepper objects, for X,Y,Z respectively
 const int n_motors = 3;
 long min_step_delay = 1000;
+long ramp_time = 200000;
 Stepper* motors[n_motors];
 signed long current_pos[n_motors];
 int steps_remaining[n_motors];
@@ -21,6 +25,17 @@ int steps_remaining[n_motors];
 // We'll use this input buffer for serial comms
 const int INPUT_BUFFER_LENGTH = 64;
 char input_buffer[INPUT_BUFFER_LENGTH];
+
+int command_prefix(String command, char ** prefixes, int n_prefixes){
+  // Check if the command starts with any of the prefixes in the list.
+  // returns the index if so, otherwise returns -1
+  for(int i=0; i<n_prefixes; i++){
+    if(command.startsWith(prefixes[i])){
+      return i;
+    }
+  }
+  return -1;
+}
 
 void setup() {
   // initialise serial port
@@ -32,7 +47,7 @@ void setup() {
   motors[1] = new Stepper(8, 9, 8, 7, 6);
   motors[2] = new Stepper(8, 5, 4, 3, 2);
   
-  for(int i=0; i<n_motors; i++){
+  EACH_MOTOR{
     motors[i]->setSpeed(1*8.0/4096.0); //using Fergus's speed for now, though this is ignored...
     steps_remaining[i]=0;
     current_pos[i]=0;
@@ -50,6 +65,15 @@ void releaseMotor(int motor){
   motors[motor]->stepMotor(8);
 }
 
+void print_position(){
+  EACH_MOTOR{
+    if(i>0) Serial.print(" ");
+    Serial.print(current_pos[i]);
+  }
+  Serial.println();
+}
+
+
 void loop() {
   // wait for a serial command and read it
   int received_bytes = Serial.readBytesUntil('\n',input_buffer,INPUT_BUFFER_LENGTH-1);
@@ -58,60 +82,96 @@ void loop() {
     String command = String(input_buffer);
 //    Serial.println("Got command: <"+command+">");
 
-    bool single_axis_move = false;
-    int motor = -1;
-    if(command.startsWith("mrx ")){
-      single_axis_move = true;
-      motor = 0;
-    }
-    if(command.startsWith("mry ")){
-      single_axis_move = true;
-      motor = 1;
-    }
-    if(command.startsWith("mrz ")){
-      single_axis_move = true;
-      motor = 2;
-    }
-    if(single_axis_move){
+    const char* single_axis_moves[3] = {"mrx ", "mry ", "mrz "};
+    int axis = command_prefix(command, single_axis_moves, 3);
+    if(axis >= 0){
       int preceding_space = command.indexOf(' ',0);
-      if(preceding_space<0){
-        Serial.println("Error: command is mr(x|y|z) <int>");
-        return;
-      }
+      if(preceding_space <= 0) Serial.println("Bad command.");
       int n_steps = command.substring(preceding_space+1).toInt();
-      int dx = n_steps > 0 ? 1 : -1;
-      n_steps *= dx; //make this number positive
+      int dx = n_steps > 0 ? 1 : -1; //find the direction
+      n_steps *= dx; //make the number of steps positive
       for(int i=0; i<n_steps; i++){
-        stepMotor(motor, dx);
+        stepMotor(axis, dx);
         delayMicroseconds(min_step_delay);
       }
       Serial.println("done");
     }
     if(command.startsWith("move_rel ") or command.startsWith("mr ")){ //relative move
       int preceding_space = -1;
-      for(int i=0; i<n_motors; i++){ //read three integers and store in steps_remaining
+      int displacement[n_motors];
+      int dir[n_motors];
+      EACH_MOTOR{ //read three integers and store in steps_remaining
         preceding_space = command.indexOf(' ',preceding_space+1);
         if(preceding_space<0){
           Serial.println("Error: command is mr <int> <int> <int>");
           break;
         }
-        steps_remaining[i] = command.substring(preceding_space+1).toInt();
+        displacement[i] = command.substring(preceding_space+1).toInt();
       }
+
+      // split displacements into magnitude and direction, and find max. travel
+      int max_steps = 0;
+      EACH_MOTOR{
+        dir[i] = displacement[i] > 0 ? 1 : -1;
+        displacement [i] *= dir[i];
+        if(displacement[i]>max_steps) max_steps=displacement[i];
+      }
+      // scale the step delays so the move goes in a straight line
+      float step_delay[n_motors];
+      EACH_MOTOR if(displacement[i]>0){
+        step_delay[i] = float(max_steps)/float(displacement[i])*float(min_step_delay);
+        //Serial.print(step_delay[i]);
+        //Serial.print(" ");
+      }else{
+        //Serial.print("x ");
+        step_delay[i] = 9999999999;
+      }
+      //Serial.println();
+      // actually make the move
+      bool finished = false;
+      int distance_moved[n_motors];
+      EACH_MOTOR distance_moved[i] = 0;
+      float start = (float) micros();
+      float final_scaled_t = (float) max_steps * min_step_delay; //NB total time taken will be final_scaled_t + 2*ramp_time
+      while(!finished){
+        float elapsed_t = (float) micros() - start;
+        float scaled_t; //scale time to allow for acceleration
+        float remaining_t = final_scaled_t + ramp_time - elapsed_t;
+        if(elapsed_t < ramp_time){ //for the first ramp_time, gradually accelerate
+          scaled_t = elapsed_t*elapsed_t/(2*ramp_time);
+        }else if(remaining_t < ramp_time){ 
+          scaled_t = final_scaled_t - remaining_t*remaining_t/(2*ramp_time);
+        }else{
+          scaled_t = elapsed_t - ramp_time/2;
+        }
+        finished = true;
+        EACH_MOTOR{
+          if(distance_moved[i] < displacement[i]){
+            finished = false; //only if all axes are done are we truly finished.
+            
+            // check if it's time to take another step and move if needed.
+            if(scaled_t > ((float)distance_moved[i] + 0.5) * step_delay[i]){
+              stepMotor(i, dir[i]);
+              distance_moved[i]++;
+              //print_position();
+            }
+          }
+        }
+        //delayMicroseconds(2000);
+      }
+      Serial.println("done.");
     }
 
     if(command.startsWith("release")){ //release steppers (de-energise coils)
-      for(int i=0; i<n_motors; i++) releaseMotor(i);
+      EACH_MOTOR releaseMotor(i);
+      Serial.println("motors released");
     }
     if(command.startsWith("p?") or command.startsWith("position?")){
-      for(int i=0; i<n_motors; i++){
-        if(i>0) Serial.print(" ");
-        Serial.print(current_pos[i]);
-      }
-      Serial.println();
+      print_position();
     }
 ///      
   }else{
-    delay(5);
+    delay(1);
     return;
   }
 }
--- python_code/test.py
+++ python_code/test.py
@@ -8,9 +8,24 @@ Created on Sun Jun 18 21:06:04 2017
 import nplab
 import nplab.instrument.serial_instrument
 import time
+from basic_serial_instrument import BasicSerialInstrument, QueriedProperty
+import numpy as np
 
-class OpenFlexureStage(nplab.instrument.serial_instrument.SerialInstrument):
+class OpenFlexureStage(BasicSerialInstrument):
     port_settings = {'baudrate':115200}
+    position = QueriedProperty(get_cmd="p?", response_string=r"%d %d %d")
+    
+    def move_rel(self, displacement, axis=None):
+        if axis is not None:
+            assert axis in ['x', 'y', 'z'], "Axis must be x, y, or z"
+            self.query("mr{} {}".format(axis, int(displacement)))
+        else:
+            #TODO: assert displacement is 3 integers
+            self.query("mr {} {} {}".format(*list(displacement)))
+    
+    def release_motors(self):
+        """De-energise the stepper motor coils"""
+        self.query("release")
 
 s = OpenFlexureStage('COM4')
 
@@ -20,10 +35,24 @@ time.sleep(1)
 #time.sleep(1)
 #print s.query("mrx -1000")
 
-for axis in ['x', 'y', 'z']:
-    for move in [-512, 512, 4096, -4096]:
+#first, try a bunch of single-axis moves
+for axis in []:#['x', 'y', 'z']:
+    for move in [-512, 512, 1024, -1024]:
         print "moving {} by {}".format(axis, move)
         qs = "mr{} {}".format(axis, move)
         print qs + ": " + s.query(str(qs))
-        print "Position: {}".format(s.query("p?"))
+        print "Position: {}".format(s.position)
+
+time.sleep(0.5)
+
+#next, describe a circle with the X and Y motors.  This is a harder test!
+radius = 1024;
+for a in np.linspace(0, 2*np.pi, 50):
+    oldpos = np.array(s.position)
+    print "Position: {}".format(oldpos)
+    newpos = np.array([np.cos(a), np.sin(a), 0]) * radius
+    displacement = newpos - oldpos
+    s.move_rel(list(displacement))
+    
+
 s.close()
