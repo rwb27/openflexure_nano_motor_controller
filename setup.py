29bac5e5796d26738ce5f559393fc375af4bafb1
Richard Bowman <richard.bowman@cantab.net>
2018-02-12 17:46:16 +0000

    Sorted packaging info

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 __author__ = 'Richard Bowman'
 
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setup(name='openflexure_stage',
-      version='0.1',
-      description='Control scripts for the OpenFlexure Microscope',
-      url='http://www.github.com/rwb27/openflexure_nano_motor_controller',
-      author='Richard Bowman',
-      author_email='r.w.bowman@bath.ac.uk',
-      license='GNU GPL v3',
-      packages=find_packages(),
-      zip_safe=True)
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name = 'openflexure_stage',
+      version = '0.1.1',
+      description = 'Control scripts for the OpenFlexure Nano Motor Controller',
+      long_description = long_description,
+      url = 'http://www.github.com/rwb27/openflexure_nano_motor_controller',
+      author = 'Richard Bowman',
+      author_email = 'r.w.bowman@bath.ac.uk',
+      download_url = 'https://github.com/rwb27/openflexure_nano_motor_controller/archive/v0.1.1.tar.gz',
+      packages = find_packages(),
+      keywords = ['arduino','serial','microscope'],
+      zip_safe = True,
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 2.7'
+          ],
+      install_requires = [
+          'pyserial'
+          ],
+      )
+
